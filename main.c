cb81f241b69f58328674de022d76efbf5f2150da
chamabayroune <bayrounec640@gmail.com>
2022-09-07 11:16:25 +0100

    calculate age

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    int year,age;
+     printf("enter year of birth:");
+    scanf("%d",&year);
+    age =2022- year;
+    printf("you have %d years old",age);
+    return 0;
+}
