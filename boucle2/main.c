1fd3f0ed067f0bf079a8966e5781708b9b16d1a9
chamabayroune <bayrounec640@gmail.com>
2022-09-09 09:10:41 +0100

    la suit de dix nombre

---
+++ boucle2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int N,i;
+             printf("entere un nombre :");
+             scanf("%d",&N);
+     for (i=N ; i <= N+10 ;i++){
+             printf("%d \n",i);
+}
+    return 0;
+}
