5c0bf2321c52a2154b27712e1a7d5a29765382e0
chamabayroune <bayrounec640@gmail.com>
2022-09-13 14:44:42 +0100

    declare,remplisse et affiche un tableau

---
+++ tableau/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+void nombre (int N){
+      int tab [10]={};
+    int i;
+    int F=0;
+    for (i=0;i<10;i++)
+        F =tab [i];
+      for (i=0;i<10;i++)
+    printf(" tab[%d] =%d ",i,tab[i]);
+}
+
+int main()
+{
+    int F ;
+  nombre(F);
+    return 0;
+}
